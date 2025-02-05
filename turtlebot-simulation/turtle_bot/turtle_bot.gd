0dfeb4179d6105ff2abea442198a61b42d1d386d
kakcalu13 <kevin.a.araujo@gmail.com>
2025-02-05 14:43:18 -0500

    updated latest

--- turtlebot-simulation/turtle_bot/turtle_bot.gd
+++ turtlebot-simulation/turtle_bot/turtle_bot.gd
@@ -15,6 +15,10 @@ func _ready():
 func get_motion_data(data: FEAGI_Data_MotionControl):
 	global_data = data
 	return data
+	
+func adjust_wheel_force(wheel, delta = 0.1):
+	if wheel.engine_force != 0.0:
+		wheel.engine_force += -delta if wheel.engine_force > 0 else delta
 
 func _physics_process(delta: float):
 	left_wheel.set_brake(0.01)
@@ -23,17 +27,23 @@ func _physics_process(delta: float):
 	var right = 0
 	
 	if global_data:
-		if global_data.move_forward or global_data.move_backward:
-			left = global_data.move_forward
+		if global_data.move_up:
+			left = global_data.move_up /2
 			right = left
-		if global_data.move_right:
+		if global_data.move_down:
+			left = (global_data.move_down /2) * -1.0
+			right = left
+		if global_data.move_left: # flipped but whatever
 			right = (global_data.move_right) / 2
 			left = (left - global_data.move_right) / 2
-		if global_data.move_left:
+		if global_data.move_right:
 			left = (global_data.move_left) / 2
 			right = (right - global_data.move_left) / 2
 		#left = (left + Input.get_axis("rotate_left", "rotate_right")) / 2.0
 		#right = (right + Input.get_axis("rotate_right", "rotate_left")) / 2.0
 		left_wheel.engine_force = left * engine_force_value
 		right_wheel.engine_force = right * engine_force_value
-	global_data = null 
+	else:
+		adjust_wheel_force(left_wheel)
+		adjust_wheel_force(right_wheel)
+	global_data = null
